9540140f7e4d0465d3668a9d33f16e9ce75c7417
Stefano Pisano <s.pisano@techgap.it>
2024-11-25 00:04:53 +0100

    feat: iproved scraping

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
 import {useEffect, useState} from "react";
+import ProgramComponent from "./Components/Program/ProgramComponent";
 
 function App() {
 
-    const [tvShows, setTvShows] = useState([]); // Initialize with an empty array
+    const [tvShows, setTvShows] = useState(new Map());
 
 
     useEffect(() => {
@@ -11,7 +12,7 @@ function App() {
         const fetchAndParseShows = async () => {
             const proxyUrl = "https://cors-anywhere.herokuapp.com/"
             const source = "https://www.staseraintv.com/programmi_stasera_"
-            const channels = ['nove', 'canale5']
+            const channels = ['rai1', 'rai2', 'rai3', 'rete4', 'canale5', 'italia1', 'la7', 'tv8', 'nove', 'realtime', 'dmax']
             try {
                 // Fetch all pages concurrently
                 const responses = await Promise.all(channels.map(ch => fetch(`${proxyUrl}${source}${ch}.html`)));
@@ -27,19 +28,19 @@ function App() {
                 const htmls = await Promise.all(responses.map(response => response.text()));
 
                 // Create a temporary DOM element to parse the HTML
-                const showsArray = [];
+                const showsMap = new Map();
 
                 htmls.forEach(html => {
                     const tempDiv = document.createElement('div');
-                    tempDiv.innerHTML = html;
+                    tempDiv.innerHTML = html.replaceAll("img", "");
 
                     const channelName = Array.from(tempDiv.getElementsByTagName("h1"))[0].innerText;
 
                     // Assuming the shows are in an <h4> element with a specific ID or class
                     const showListElement = tempDiv.querySelector('h4'); // Adjust the selector as needed
                     if (showListElement) {
-                        const shows = Array.from(showListElement.querySelectorAll('br'))
-                            .map((br, index) => showListElement.childNodes[index].textContent.trim())
+                        const shows = Array.from(tempDiv.querySelector('h4').innerHTML.split("<br>"))
+                            .map((content, index) => content.trim())
                             .filter(show => show !== '')
                             .map(show => {
                                 const info = show.split(" - ");
@@ -51,14 +52,15 @@ function App() {
                                 }
                             })
 
-                        console.log(shows)
-
-                        showsArray.push(...shows);
+                        if (showsMap.has(channelName)) {
+                            showsMap.get(channelName).push(...shows);
+                        } else {
+                            showsMap.set(channelName, shows);
+                        }
                     }
                 });
 
-                console.log(showsArray)
-                setTvShows(showsArray);
+                setTvShows(showsMap);
             } catch (error) {
                 console.error('Error fetching or parsing shows:', error);
             }
@@ -71,8 +73,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <div className="container m-auto flex items-center flex-col gap-10">
-
+          <div className="container m-auto flex items-center flex-col gap-10 mt-10 mb-10">
+                  {Array.from(tvShows.entries())
+                      .map(([channel, shows]) => <ProgramComponent key={channel} channel={channel} shows={shows}/>)}
           </div>
 
       </header>
--- src/Components/Program/ProgramComponent.tsx
+++ src/Components/Program/ProgramComponent.tsx
@@ -1,30 +1,26 @@
 import React from "react";
 import styles from "./ProgramComponent.module.css"
 
-interface ProgramProps {
-    channel:string;
+interface Show {
     title: string;
-    img:string;
-    description: string;
     startsAt: string;
-    endsAt: string;
-    duration: string;
 }
 
-const ProgramComponent: React.FC<ProgramProps> = ({ channel, title, img, description, startsAt, endsAt, duration }) => {
-    return (
-        <div className="w-2/3 bg-gray-900 rounded-lg p-10">
-            <h2 className="font-sans text-3xl text-gray-300 mb-5">{title}</h2>
-            <div className="flex flex-row gap-5">
-                <img className="basis-1/3 w-60 h-auto object-cover hidden lg:block" alt={title} src={img}/>
-                <div className="flex flex-col gap-3">
-                    <p className={`font-mono text-black-300 text-xl lg:w-1/3 rounded-lg pl-2 pr-2  ${styles.channel}`}>{channel}</p>
-                    <p className="font-mono text-gray-300 text-xs text-justify">{description}</p>
-                    <p className="font-mono text-gray-300"><span className={"font-bold"}>Starts at:</span> {startsAt} <span className={"font-bold"}>Ends at:</span>{endsAt}</p>
-                    <p className="font-mono text-gray-300"><span className={"font-bold"}>Duration:</span> {duration}</p>
-                </div>
+interface ProgramProps {
+    channel:string;
+    shows: Show[]
+}
 
-            </div>
+
+const ProgramComponent: React.FC<ProgramProps> = ({ channel, shows}) => {
+    return (
+        <div className="w-2/5 bg-gray-900 rounded-lg p-10">
+            <h2 className="font-sans text-3xl text-gray-300 mb-5">{channel}</h2>
+                    {
+                        shows.map(((show, i) =>
+                            <p key={i} className={`font-mono text-black-300 pl-2 pr-2 text-gray-100`}>{show.startsAt} - {show.title}</p>
+                        ))
+                    }
 
         </div>
     );
