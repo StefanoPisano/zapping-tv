c59c839c60fbdfb0d1af74e07382d59f9ecf2577
Stefano Pisano <s.pisano@techgap.it>
2024-12-12 21:42:38 +0100

    fix: proxy url

--- src/constants/scraper.ts
+++ src/constants/scraper.ts
@@ -1,4 +1,4 @@
-const proxy = "https://corsproxy.io/?";
+const proxy = "https://corsproxy.io/?key=1566f6f4&url=";
 const source = "https://www.staseraintv.com/programmi_stasera_";
 
 export {proxy, source}
